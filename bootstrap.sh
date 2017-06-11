b8ccd2128a1129d3c5af65cf7f51c037e9789504
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2017-06-11 12:22:40 -0500

    Build RPM package locally

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+(
+	autoreconf -fvi
+	./configure
+	make dist
+	rm -rf RPM
+	mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
+	rpmbuild							\
+		-D"_topdir ${PWD}/RPM"					\
+		-D"_sourcedir ${PWD}"					\
+		-D"_specdir ${PWD}"					\
+		-ba							\
+		signature.spec
+) 2>&1 | tee bootstrap.log
